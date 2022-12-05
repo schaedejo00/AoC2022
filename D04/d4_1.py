97f162d1b250ddb0b0d9b02786f1944e682c3a45
schaedejo00 <j.schaede@gymnasium-oberstadt.de>
2022-12-05 16:09:46 +0100

    day4

---
+++ D04/d4_1.py
@@ -0,0 +1,20 @@
+from typing import Set
+
+import numpy as np
+
+count = 0
+with open('input_1.txt', 'r') as f:
+    line = f.readline().replace("\n","")
+    while line:
+        line = [token.split("-") for token in line.split(",")]
+        left: Set[int] = set(np.arange(int(line[0][0]), int(line[0][1])+1, 1))
+        right: Set[int] = set(np.arange(int(line[1][0]), int(line[1][1]) + 1, 1))
+
+        intersection: Set[int] = left & right
+        if left.issubset(intersection) or right.issubset(intersection):
+            count+=1
+
+        print(line, left, right, intersection)
+        line = f.readline().replace("\n","")
+print (count)
+
